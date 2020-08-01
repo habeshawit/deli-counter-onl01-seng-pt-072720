2519812c2e66dfa7ce7a9b4063467c2779b1e2b4
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-01 02:10:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,9 +18,8 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    while katz_deli.length !=0
-      puts "Currently serving #{katz_deli[0]}."
-      katz_deli.shift
-    end
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+    
   end
 end
