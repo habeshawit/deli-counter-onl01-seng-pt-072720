5e300cc782018241a0583157c1e7a72004bec19d
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-01 02:11:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    new_array = katz_deli.each_with_index.map {|customer, index| "#{index+1}. #{customer}"}
+    puts "The line is currently: #{new_array.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli,new_customer)
+  katz_deli.push(new_customer)
+  puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
