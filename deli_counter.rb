b21b049339bd832781012f88b5396ae93c8c785c
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-01 02:07:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,15 @@ end
 def take_a_number(katz_deli,new_customer)
   katz_deli.push(new_customer)
   puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
-  
-  
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    while katz_deli.length !=0
+      puts "Now serving: #{katz_deli[0]}"
+      katz_deli.shift
+    end
+  end
 end
