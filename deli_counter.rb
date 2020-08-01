9f180bc6456afcc988979f47b757c4c6522a6093
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-01 01:37:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{katz_deli.each_with_index.map{|customer, index| #{index+1}. #{customer}}}"
+  end
+    
+    #katz_deli.each_with_index{|customer, index| puts "#{index+1}. #{customer}"}
+    
+    #katz_deli.each_with_index.map{|customer, index| "#{index+1}. #{customer}"}
+  
+    
+end
